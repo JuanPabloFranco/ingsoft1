b56f6a0fc1925825a0e93e59e199d69314c0bd68
JP_PC <JP_PC@DESKTOP-OSCFP9R>
2020-08-12 17:41:40 -0500

    Organización de directorios y librerias iniciales

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include 'Modelo/clsConexion.php';
+$conexion = new clsConexion();
+$fecha = date("Y") . "-" . date("m") . "-" . date("d");
+$año = date("Y");
+$mes = date("m");
+$dia = date("d");
+?>
+<html>
+    <head>
+        <meta charset="UTF-8"> 
+        <meta name="google-site-verification" content="uH5l5ImkziBtdea5yssScH2VMJqSIpS3VmPfhzwZUt8" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script language="javascript" src="Recursos/js/jquery-3.1.1.min.js"></script>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+        <meta http-equiv=Content-Type content="text/html; charset=ISO-8859-1">
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />        
+        <script type="text/javascript" src="Recursos/js/gestionLogin.js"></script>
+        <script src="Recursos/js/jquery-3.2.1.js" type="text/javascript"></script>
+        <link rel="StyleSheet" href="Recursos/css/estilos.css" type="text/css">
+        <link rel="shortcut icon" type="image/x-icon" href="Recursos/imagenes/casaFesanco.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+    </head>
+    <body>
+        <div class="masterPage">
+            <?php
+            include 'Vista/masterPage.php';
+            ?>
+        </div>        
+    </body>
+</html>
